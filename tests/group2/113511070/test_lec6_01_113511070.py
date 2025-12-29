94c8845df127353f6b4eb31b6bba5e87a12d614c
ginny923 <liulichun0923.ee13@nycu.edu.tw>
2025-12-29 14:05:49 +0800

    Add lec6 Hanoi pytest

--- tests/group2/113511070/test_lec6_01_113511070.py
+++ tests/group2/113511070/test_lec6_01_113511070.py
@@ -1,4 +1,9 @@
-import pytest
+import os
+import sys
+
+# 確保可 import 同資料夾的 lec6_recursion_dictionaries.py
+sys.path.append(os.path.dirname(__file__))
+
 from lec6_recursion_dictionaries import Towers
 
 
@@ -16,8 +21,3 @@ def test_hanoi_n2_prints_three_moves(capsys):
         "move from P1 to P2",
         "move from P3 to P2",
     ]
-
-
-def test_hanoi_rejects_non_positive():
-    with pytest.raises(ValueError):
-        Towers(0, "P1", "P2", "P3")
